bc1ff9e850a89e18956d2d279ac1e3d3bcfea55d
Ayush Bansal <ayushb6732@gmail.com>
2023-08-26 17:48:37 +0530

    Finished consumer and producer in kafka

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -8,3 +8,11 @@ consumer = KafkaConsumer(
     enable_auto_commit=True,
     value_deserializer=lambda K: json.loads(K.decode('utf-8'))
     )
+
+if __name__ == "__main__":
+    try:
+        for data in consumer:
+            json_data = json.loads(data.value)
+            print(json_data)
+    except KeyboardInterrupt:
+        print('Consumer closed!')
